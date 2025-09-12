c6a134893ac42ed3242ce4b9c277ef7f57a4b36f
pallavi-044 <pallavigangula3@gmail.com>
2025-09-12 13:45:56 +0530

    Create 7.py

---
+++ 7.py
@@ -0,0 +1,26 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = set()
+    queue = deque([start])
+
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            print(node, end=" ")
+            visited.add(node)
+            queue.extend(graph[node])
+
+# Example graph (Adjacency List)
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+# Run BFS
+print("BFS Traversal starting from A:")
+bfs(graph, 'A')
