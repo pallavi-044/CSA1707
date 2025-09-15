6d8c9eba05779df1f82f75176aeb0e439115aa21
pallavi-044 <pallavigangula3@gmail.com>
2025-09-15 14:38:04 +0530

    Create 29.pl

---
+++ 29.pl
@@ -0,0 +1,9 @@
+% facts
+employee(alice, sales).
+employee(bob, tech).
+employee(carol, manager).
+
+% rules
+access(X, report) :- employee(X, sales).
+access(X, server) :- employee(X, tech).
+approve(X) :- employee(X, manager).
