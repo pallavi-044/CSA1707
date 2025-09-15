d3281af9932af96ad9455365a242ac6a381990e1
pallavi-044 <pallavigangula3@gmail.com>
2025-09-15 14:39:09 +0530

    Create 30.pl

---
+++ 30.pl
@@ -0,0 +1,9 @@
+% --- Facts ---
+employee(alice, sales).
+employee(bob, tech).
+employee(carol, manager).
+
+% --- Rules ---
+access(X, report) :- employee(X, sales).
+access(X, server) :- employee(X, tech).
+approve(X) :- employee(X, manager).
